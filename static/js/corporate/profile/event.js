a04d11740eed31bb0df3fa0d7c1030166aade7da
youmin92433 <youmin92433@gmail.com>
2026-01-24 15:18:33 +0900

    update

--- static/js/corporate/profile/event.js
+++ static/js/corporate/profile/event.js
@@ -3,6 +3,9 @@
 const logoUploadButton = document.getElementById("devLogoUp");
 const logoUploadLayout = document.querySelector(".tbCell.tbLogo");
 
+const logoUploadModal = document.querySelector(".popLogoUpload");
+const logoDeleteModal = document.querySelector(".popLogoDel");
+
 // 닫기
 const logoUploadCloseButton = document.getElementById("devCloseLayer");
 const inputs = document.querySelectorAll("input");
@@ -11,8 +14,11 @@ const textareas = document.querySelectorAll("textarea");
 // 로고 선택
 const selectLogoInput = document.getElementById("devSelectLogo");
 const selectLogoLabel = document.querySelector("#devSelectLogoDelegator label");
+let logoImage = null;
 
-console.log(selectLogoLabel);
+// 로고 업로드
+const logoUploadSubmitButton = document.getElementById("devSubmit");
+const logoContainer = document.querySelector(".tbCell.tbLogo");
 
 // 이벤트
 // 닫기
@@ -26,13 +32,13 @@ textareas.forEach((textarea) => {
         logoUploadLayout.classList.remove("on");
     });
 });
-
-// 열기
 logoUploadCloseButton.addEventListener("click", (e) => {
     logoUploadLayout.classList.remove("on");
 });
 
+// 열기
 logoUploadButton.addEventListener("click", (e) => {
+    logoUploadModal.classList.add("on");
     logoUploadLayout.classList.add("on");
 });
 
@@ -41,14 +47,57 @@ selectLogoInput.addEventListener("change", (e) => {
     const [file] = e.target.files;
     const reader = new FileReader();
 
-    selectLogoLabel.textContent = file.name;
-
     reader.readAsDataURL(file);
-    reader.addEventListener("load", (e) => {
-        const path = e.target.result;
 
-        if (path.includes("image")) {
-            // 로고 업로드
+    reader.addEventListener("load", (e) => {
+        logoImage = e.target.result;
+
+        const check = ["gif", "jpeg", "jpg", "png"].some((extension) =>
+            logoImage.includes(extension),
+        );
+
+        if (!check) {
+            selectLogoLabel.textContent = "";
+            alert("파일 형식이 올바르지 않습니다.");
+            return;
         }
+        selectLogoLabel.textContent = file.name;
     });
+});
+
+// 로고 업로드
+logoUploadSubmitButton.addEventListener("click", (e) => {
+    console.log("들어옴");
+    if (logoImage) {
+        logoContainer.firstElementChild.remove();
+        logoContainer.innerHTML += `
+            <p class="logo">
+                <img src="${logoImage}">
+            </p>
+            <p class="btn">
+                <button type="button" class="infoBtn infoBtnDel" id="devLogoDel"></button>
+                <button type="button" class="infoBtn infoBtnMod" id="devLogoModify"></button>
+            </p>
+        `;
+        logoUploadLayout.classList.remove("on");
+
+        alert("업로드 되었습니다.");
+    } else {
+        alert("로고를 선택해 주십시오.");
+    }
+});
+
+// 로고 업로드 후
+logoUploadLayout.addEventListener("click", (e) => {
+    if (e.target.classList.contains("infoBtnDel")) {
+        // logoUploadModal.classList.remove("on");
+
+        logoDeleteModal.classList.add("on");
+        logoUploadLayout.classList.add("on");
+    } else if (e.target.classList.contains("infoBtnMod")) {
+        logoDeleteModal.classList.remove("on");
+        logoUploadModal.classList.add("on");
+
+        logoUploadLayout.classList.add("on");
+    }
 });
